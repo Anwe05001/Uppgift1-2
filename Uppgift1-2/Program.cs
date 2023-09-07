116d609d8fd04e650fe2b51a460f2c6d93476037
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:14:31 +0200

    Add project files.

---
+++ Uppgift1-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.WriteLine("YOU HAVE ENCOUNTERED A GRAVE ERROR");
+                Console.WriteLine("ERROR CODE 404");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
